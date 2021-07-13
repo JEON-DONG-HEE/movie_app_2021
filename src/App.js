fc1bfadfc20e724bbc1dd5f7c06cb7f0ca098445
JEON-DONG-HEE <bethetopone@gmail.com>
2021-07-13 14:41:11 +0900

    210713_01 JSX & Props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
 
-function Food({favourite}) {
-  return <h1>나는 {favourite} 를 좋아한다.</h1>
+const listFood = [
+  {
+    key: 1,
+    name : "닭가슴살 페퍼",
+    image : "https://www.pocketsalad.co.kr/data/goods/20/08/10/447/447_detail_029.jpg"
+  },
+  {
+    key: 2,
+    name : "방울토마토",
+    image : "https://health.chosun.com/site/data/img_dir/2014/03/27/2014032702818_0.jpg"
+  },
+  {
+    key: 3,
+    name : "아몬드 브리즈",
+    image : "https://newsimg.hankookilbo.com/cms/articlerelease/2015/04/07/201504071481308694_1.jpg"
+  },    
+];
+
+function Food({name, picture}) {
+  return <div>
+    <h1>나는 {name} 을 좋아한다</h1><img src={picture} alt={name} />
+  </div>
 }
 
 function App() {
   return (
     <div>
-      Hello!! Dong HEE
-      <Food
-        favourite = "닭가슴살 페퍼"
-      />
-      <Food
-        favourite = "방울토마토"
-      />
-      <Food
-        favourite = "아몬드 브리즈"
-      />
+      {listFood.map(item => (
+        <Food key={item.key} name={item.name} picture={item.image} />
+      ))}   
     </div>
   );
 }
