098d61f21e128bddc7975e7dd790a4e72c81f9e0
JEON-DONG-HEE <bethetopone@gmail.com>
2021-07-31 15:52:31 +0900

    210731_04 First step for Movie app

--- src/App.js
+++ src/App.js
@@ -2,42 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component {
-
-  constructor(props) {  // 컴포넌트 생명주기 1 : 컴포턴트가 Virtual Dom 에 삽입될 때에 호출
-    super(props);
-    console.log("The component did inserted, Nico see this haha");
-  }
-
   state = {
-    count: 1,
+    isLoading : true,    
   };
 
-  plus = () => {
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
-  };
-
-  componentDidMount() { // 컴포넌트 생명주기 3 : 컴포턴트 render 가 완료되었을 때 호출
-    console.log("render complited");
-  }
-
-  componentDidUpdate() {  // 컴포넌트 생명주기 4 : 컴포턴트가 Udate 되었을 때 호출(state 변경 등)
-        console.log("The component did updated");
-  }
-
-  componentWillUnmount() {  // 컴포넌트 생명주기 5 : 컴포넌트가 DOM 상에서 제거될 때 호출(컴포넌트 소스가 변경된 후 저장될 때 호출)
-    console.log("Removed component");
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({isLoading : false})
+    }, 6000);
   }
 
   render() {
-    console.log("render"); // 컴포넌트 생명주기 2 : 컴포턴트가 render 될 때 호출
+    const {isLoading} = this.state;
     return (
       <div>
-        <p>방울 토마토의 갯수 : {this.state.count}</p>
-        <button onClick={this.plus}>Plus</button>
-        <button onClick={this.minus}>Minus</button>
+        {isLoading ? "Loading..." : "We are ready"}
       </div>
     )
   }
